2b27fd9d29e3a382249f690418663d18e84d28bd
realsun1 <sundin.nguyen@gmail.com>
2024-07-04 21:19:41 -0400

    Implement the complete layout

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const SignIn = () => {
   return (
-    <div>page</div>
+    <div>SignIn</div>
   )
 }
 
-export default page
+export default SignIn
